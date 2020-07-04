5aa6c30154c5a649d3bf308b25044f4e37652e84
Almog Bakhshi <AlmogBakhshi@gmail.com>
2020-07-05 02:47:32 +0300

    new build

---
+++ precache-manifest.e078f9463e57af0bd67f413e902758d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5de6919b139ea8b2b35ab11f355bcde",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a97d1e020fbd559adc7",
+    "url": "./static/css/main.d1aeb4d7.chunk.css"
+  },
+  {
+    "revision": "43e14992b3a3fb3ee4c3",
+    "url": "./static/js/2.4692a386.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "./static/js/2.4692a386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a97d1e020fbd559adc7",
+    "url": "./static/js/main.93b671ae.chunk.js"
+  },
+  {
+    "revision": "7c7f2ce068e56fd437a7",
+    "url": "./static/js/runtime-main.b8a268a3.js"
+  },
+  {
+    "revision": "69b3d35b79e2726700913ca3928fde46",
+    "url": "./static/media/tembleweed.69b3d35b.png"
+  },
+  {
+    "revision": "6d513c48ea29df042109c997810b3bb8",
+    "url": "./static/media/wildwest-bg.6d513c48.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36d10593391a10647058d9f7bc70ea86.js"
+  "./precache-manifest.e078f9463e57af0bd67f413e902758d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
